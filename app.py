8f35669dd2b4d890afa4ead841027b646457ca6a
fabien-mrccc <fabienmarcuccini.pro@gmail.com>
2024-03-29 11:49:07 +0100

    add / route

--- app.py
+++ app.py
@@ -2,3 +2,12 @@ from flask import Flask, session, request, redirect, render_template
 import data_model as model
 
 app = Flask(__name__)
+
+
+########################
+#      GET ROUTES      #
+########################
+
+@app.route('/')
+def home(): 
+    render_template('accueil.html')
