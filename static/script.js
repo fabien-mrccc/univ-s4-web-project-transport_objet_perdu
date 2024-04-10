3b330b9879d487dc929cbec8f22bfa97b44bf3ea
MARCUCCINI Fabien <fabien.marcuccini@etu.univ-amu.fr>
2024-04-10 14:34:40 +0200

    add disconnect button

--- static/script.js
+++ static/script.js
@@ -6,15 +6,33 @@ function main() {
 
     let leftHomeButton = document.getElementById('leftHomeButton');
     let rightHomeButton = document.getElementById('rightHomeButton');
+    let disconnectButton = document.getElementById('disconnectButton');
 
     buttonEventListener(leftHomeButton, "/recuperer-objet-perdu");
     buttonEventListener(rightHomeButton, "/ma-compagnie-de-transport");
+    buttonEventListener(disconnectButton, "/deconnexion-compagnie-transport", true);
 }
 
-function buttonEventListener(button, page) {
-    button.addEventListener('click', function() {
-        redirect(page);
-    });
+function buttonEventListener(button, page, isPost = false) {
+    if (button != null) {
+        button.addEventListener('click', function() {
+            
+            if (isPost) {
+                postRedirect(page);
+            }
+            else {
+                redirect(page);
+            }
+        });
+    }
+}
+
+function postRedirect(page) {
+    let form = document.createElement('form');
+    document.body.appendChild(form);
+    form.method = 'post';
+    form.action = page;
+    form.submit();
 }
 
 function redirect(page) {
