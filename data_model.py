fa45be70a5331b4eeb854451f6c8bf0dc898999b
aminafanani <amina.fanani@etu.univ-amu.fr>
2024-04-10 17:24:38 +0200

    add data_model.py's utility functions code

---
+++ data_model.py
@@ -0,0 +1,45 @@
+import sqlite3
+from werkzeug.security import check_password_hash, generate_password_hash
+import math
+
+DBFILENAME = 'companies.sqlite'
+
+# Utility functions
+def db_fetch(query, args=(), all=False, db_name=DBFILENAME):
+  with sqlite3.connect(db_name) as conn:
+    # to allow access to columns by name in res
+    conn.row_factory = sqlite3.Row 
+    cur = conn.execute(query, args)
+    # convert to a python dictionary for convenience
+    if all:
+      res = cur.fetchall()
+      if res:
+        res = [dict(e) for e in res]
+      else:
+        res = []
+    else:
+      res = cur.fetchone()
+      if res:
+        res = dict(res)
+  return res
+
+def db_insert(query, args=(), db_name=DBFILENAME):
+  with sqlite3.connect(db_name) as conn:
+    cur = conn.execute(query, args)
+    conn.commit()
+    return cur.lastrowid
+
+
+def db_run(query, args=(), db_name=DBFILENAME):
+  with sqlite3.connect(db_name) as conn:
+    cur = conn.execute(query, args)
+    conn.commit()
+    return cur
+
+
+def db_update(query, args=(), db_name=DBFILENAME):
+  with sqlite3.connect(db_name) as conn:
+    cur = conn.execute(query, args)
+    conn.commit()
+    return cur.rowcount
+  
