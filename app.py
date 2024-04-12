83dddef2e3d1e51462a6298dcc3f8616e800eb3a
MARCUCCINI Fabien <fabien.marcuccini@etu.univ-amu.fr>
2024-04-12 11:49:41 +0200

    remove loged_in method

--- app.py
+++ app.py
@@ -1,15 +1,8 @@
 from flask import Flask, session, request, redirect, render_template, url_for
 import data_model as model
 
-
 app = Flask(__name__)
 app.secret_key = b'6dbb6b3863634aa6a72270de16df48e666f2564fddcc5fe3c27effe4393a7f4b'
-
-def loged_in():
-    if not('email' in session):
-        return False
-    else:
-        return True
 
 
 ########################
@@ -26,14 +19,14 @@ def recover_lost_object():
 
 @app.get('/ma-compagnie-de-transport')
 def my_transport_company():
-    if(loged_in() == True):
+    if 'email' in session:
         return render_template('ma_compagnie_de_transport.html')
     else:
-        return login_get()
+        return redirect('/connexion-compagnie-transport')
 
 @app.get('/connexion-compagnie-transport')
 def login_get():
-    return render_template('connexion_compagnie_transport.html')
+    return render_template('connexion_compagnie_transport.html', email_in=False)
 
 @app.get('/inscription-compagnie-transport')
 def register_get():
@@ -54,21 +47,19 @@ def register_post():
     postal_code = request.form["postal_code"]
     try:
         model.register_company_account(name, website, email, password, city, postal_code)
-    except ValueError as err:
+    except ValueError:
         return render_template('inscription_compagnie_transport.html', email_in=True)
     return redirect('/connexion-compagnie-transport')
 
-"""
-@app.post('/delete-account')
-def delete_account():
-"""
 @app.post('/connexion-compagnie-transport')
 def login_post():
     email = request.form['email']
+    password = request.form['password']
     try:
-        model.authentification(request.form['email'], request.form['password'])
-    except ValueError as err:
-        return render_template('connexion_compagnie_transport.html')
+        model.authentification(email, password)
+    except ValueError:
+        print("catch")
+        return render_template('connexion_compagnie_transport.html', email_in=True)
     session['email'] = email
     return redirect('/ma-compagnie-de-transport')
 
@@ -77,5 +68,8 @@ def logout_post():
     session.clear()
     return redirect('/')
 
-
+"""
+@app.post('/delete-account')
+def delete_account():
+"""
 
