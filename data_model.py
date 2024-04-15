d72cb9c5d9f1677460fbaf551bb169e0aace929b
MARCUCCINI Fabien <fabien.marcuccini@etu.univ-amu.fr>
2024-04-15 11:29:36 +0200

    code cleaned for data_model.py

--- data_model.py
+++ data_model.py
@@ -3,50 +3,6 @@ from werkzeug.security import check_password_hash, generate_password_hash
 
 DBFILENAME = 'companies.sqlite'
 
-###########################
-# BEGIN Utility functions #
-###########################
-
-def db_fetch(query, args=(), all=False, db_name=DBFILENAME):
-  with sqlite3.connect(db_name) as conn:
-    # to allow access to columns by name in res
-    conn.row_factory = sqlite3.Row 
-    cur = conn.execute(query, args)
-    # convert to a python dictionary for convenience
-    if all:
-      res = cur.fetchall()
-      if res:
-        res = [dict(e) for e in res]
-      else:
-        res = []
-    else:
-      res = cur.fetchone()
-      if res:
-        res = dict(res)
-  return res
-
-def db_insert(query, args=(), db_name=DBFILENAME):
-  with sqlite3.connect(db_name) as conn:
-    cur = conn.execute(query, args)
-    conn.commit()
-    return cur.lastrowid
-
-def db_run(query, args=(), db_name=DBFILENAME):
-  with sqlite3.connect(db_name) as conn:
-    cur = conn.execute(query, args)
-    conn.commit()
-    return cur
-
-def db_update(query, args=(), db_name=DBFILENAME):
-  with sqlite3.connect(db_name) as conn:
-    cur = conn.execute(query, args)
-    conn.commit()
-    return cur.rowcount
-
-#########################
-# END Utility functions #
-#########################
-
 
 def register_company_account(company_name, website, email, password, city, department):
     
@@ -168,4 +124,49 @@ def get_recovery_sites():
                    FROM ContactInformation ci 
                    JOIN TransportCompany tc ON ci.Company_ID = tc.ID 
                    JOIN City c ON ci.City_ID = c.ID ORDER BY LOWER(tc.Name);""", all=True)
+
+
+#####################################
+#      BEGIN Utility functions      #
+#####################################
+
+def db_fetch(query, args=(), all=False, db_name=DBFILENAME):
+  with sqlite3.connect(db_name) as conn:
+    # to allow access to columns by name in res
+    conn.row_factory = sqlite3.Row 
+    cur = conn.execute(query, args)
+    # convert to a python dictionary for convenience
+    if all:
+      res = cur.fetchall()
+      if res:
+        res = [dict(e) for e in res]
+      else:
+        res = []
+    else:
+      res = cur.fetchone()
+      if res:
+        res = dict(res)
+  return res
+
+def db_insert(query, args=(), db_name=DBFILENAME):
+  with sqlite3.connect(db_name) as conn:
+    cur = conn.execute(query, args)
+    conn.commit()
+    return cur.lastrowid
+
+def db_run(query, args=(), db_name=DBFILENAME):
+  with sqlite3.connect(db_name) as conn:
+    cur = conn.execute(query, args)
+    conn.commit()
+    return cur
+
+def db_update(query, args=(), db_name=DBFILENAME):
+  with sqlite3.connect(db_name) as conn:
+    cur = conn.execute(query, args)
+    conn.commit()
+    return cur.rowcount
+
+###################################
+#      END Utility functions      #
+###################################
 
